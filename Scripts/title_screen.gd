51cc1ece3d62f8400ce4a72feab6d8549f7e638d
Jacob Schipman <JacobSchipman@hotmail.com>
2024-08-16 18:56:17 -0400

    Title transition

---
+++ Scripts/title_screen.gd
@@ -0,0 +1,11 @@
+extends Control
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn")
+	pass # Replace with function body.
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
+	pass # Replace with function body.
