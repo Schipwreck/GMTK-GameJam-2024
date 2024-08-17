399c817b2aa8565908ad357ffa9a27393c05cb19
Kenny Nguyen <khnguy@proton.me>
2024-08-17 02:00:48 -0400

    scoring skeleton
    basic score page, score counter, level incrementation

--- Scripts/main_scene_3.gd
+++ Scripts/main_scene_3.gd
@@ -1,5 +1,16 @@
 extends Node2D
 
+# callback function
+func _process(_delta):
+	# display overall score
+	$Score.text = "Score: %d" % Global.overallScore
 
-func _on_packitup_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/main_scene.tscn") # Input Results 3 scene
+func _on_packitup_button_pressed(_delta):
+	# debug msg
+	print("Level " + str(Global.levelCount) + " passed!")
+	
+	# calc score goes here..
+	Global.overallScore += 1
+	
+	# change scene
+	get_tree().change_scene_to_file("res://Scenes/score_page.tscn")
