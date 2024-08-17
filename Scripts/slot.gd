5ce9cfaaccc5d3e3c60432204bd07d9dbe543fef
dexterity74 <justin.704504@gmail.com>
2024-08-17 01:04:58 -0500

    settled consistent directory names, added proper Godot 4.x .gitignore, fixed warnings

---
+++ Scripts/slot.gd
@@ -0,0 +1,39 @@
+extends TextureRect
+
+# Ignoring "unused_signal" warning is okay.
+# Currently, if a signal is used for emit_signal() purposes only,
+# it will warning regardless because there is no way to correlate a signal
+# declaration to its emission.
+@warning_ignore("unused_signal")
+signal slot_entered(slot)
+@warning_ignore("unused_signal")
+signal slot_exited(slot)
+
+@onready var filter = $StatusFilter
+
+var slot_ID
+var is_hovering := false
+enum States {DEFAULT, TAKEN, FREE}
+var state := States.DEFAULT
+var item_stored = null
+
+func set_color(a_state = States.DEFAULT) -> void:
+	match a_state:
+		States.DEFAULT:
+			filter.color = Color(Color.WHITE, 0.2)
+		States.TAKEN:
+			filter.color = Color(Color.RED, 0.2)
+		States.FREE:
+			filter.color = Color(Color.GREEN, 0.2)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	if get_global_rect().has_point(get_global_mouse_position()):
+		if not is_hovering:
+			is_hovering = true
+			emit_signal("slot_entered", self)
+	else:
+		if is_hovering:
+			is_hovering = false
+			emit_signal("slot_exited", self)
