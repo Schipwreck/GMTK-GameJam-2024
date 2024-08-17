837a02af42888fd6feb1e2c6818176ae175e32bc
Ian Gerodias <30375169+ilgerodias@users.noreply.github.com>
2024-08-17 12:46:05 -0700

    finally finished functionality for shrink/enlarge

--- Scripts/IG-scripts/mouse_drag_drop.gd
+++ Scripts/IG-scripts/mouse_drag_drop.gd
@@ -1,13 +1,14 @@
 extends Node2D
 
 var selected = false
+var hovering = false
 
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	if selected:
 		followMouse()
 
@@ -16,7 +17,7 @@ func followMouse():
 	position = get_global_mouse_position()
 
 
-func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+func _on_area_2d_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		#mouse left click pressed
 		if event.pressed:
@@ -24,3 +25,17 @@ func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) 
 		#mouse left click released
 		else:
 			selected = false
+
+func _input(event):
+	#press S to shrink while moused over object
+	if event.is_action_pressed("shrinkRay") and hovering == true:
+		print("Entered small state")
+		self.scale = Vector2(0.5, 0.5)
+	#press E to enlarge while moused over object
+	elif event.is_action_pressed("biggerRay") and hovering == true:
+		self.scale = Vector2(1.5,1.5)
+
+
+func _on_area_2d_mouse_shape_entered(shape_idx: int) -> void:
+	hovering = true
+	print(hovering)
