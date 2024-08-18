7067e138b9c0f04b84f8751c2a54a56bc90f7056
Ian Gerodias <30375169+ilgerodias@users.noreply.github.com>
2024-08-18 15:28:25 -0700

    fine tuned functionality for grids and added actual sprites as well as cleaned up data sheet

--- Scripts/Grid/concept_ui.gd
+++ Scripts/Grid/concept_ui.gd
@@ -124,8 +124,8 @@ func _on_button_spawn_pressed() -> void:
 	
 	var new_item = item_scene.instantiate()
 	add_child(new_item)
-	#new_item.load_item(str(randi_range(1, 4)))
-	new_item.load_item(str(1))
+	#new_item.load_item(str(randi_range(1, 3)))
+	new_item.load_item(DataHandler.item_name_list.pick_random())
 	new_item.selected = true
 	item_held = new_item
 	
