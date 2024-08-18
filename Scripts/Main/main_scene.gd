af1e3b5bcab17167ef814edcce43465105f86ef7
Kenny Nguyen <khnguy@proton.me>
2024-08-18 00:36:01 -0400

    End game screen, additional level modularity, scoring modularity
    End game screen created, additional modularity added for future levels, additional score modularity for score logic to account for different weight & value modifiers for different characters

--- Scripts/Main/main_scene.gd
+++ Scripts/Main/main_scene.gd
@@ -1,5 +1,31 @@
 extends Node2D
 
+func _ready():
+	Global.character = "Granny" # add character name here
 
-func _on_packitup_button_pressed() -> void:
+# callback function
+func _process(_delta):
+	# display overall score
+	$TextureRect/MarginContainer2/Score.text = "Score: %d" % Global.overallScore
+
+func _on_packitup_button_pressed():
+	# debug message
+	print("Level " + str(Global.levelCount) + " passed!")
+	
+	# storing score in placeholder int
+	Global.scorekeeper = Global._calculateScore()
+	
+	# adding placeholder int to overall score
+	Global.overallScore += Global.scorekeeper
+	
+	# adding placeholder int to score array
+	Global.arr.push_back(Global.scorekeeper)
+	
+	# printing stars
+	Global.stars = Global._calculateStars()
+	
+	# resetting placeholder int
+	Global.scorekeeper = 0
+	
+	# change scene
 	get_tree().change_scene_to_file("res://Scenes/Score/score_page.tscn")
