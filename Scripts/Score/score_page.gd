6b2272e36f1f4fefb54f7508e92d8960fd718061
Kenny Nguyen <khnguy@proton.me>
2024-08-19 21:30:20 -0400

    Added star stuff to score page

--- Scripts/Score/score_page.gd
+++ Scripts/Score/score_page.gd
@@ -1,5 +1,14 @@
 extends Node
 
+@onready var animation = $AnimationPlayer
+
+func _ready():
+	$Score.text = "Score: %d" % Global.overallScore
+	var starPath = "res://Assets/Stars/" + str(Global.stars) + ",0.png"
+	var image = Image.load_from_file(starPath)
+	var texture = ImageTexture.create_from_image(image)
+	$Sprite2D.texture = texture
+
 # callback function
 func _process(_delta):
 	$Score.text = "Score: %d" % Global.overallScore
@@ -11,7 +20,6 @@ func _on_continue_pressed():
 	TransitionScene.transition()
 	await TransitionScene.on_transition_finished
 	var string = ""
-	Global.stars = ""
 	
 	# increment level count by 1 every time continue is pressed
 	Global.levelCount += 1
@@ -19,14 +27,12 @@ func _on_continue_pressed():
 	# to add levels, increase value below
 	if Global.levelCount == 4:
 		
+		Global.levelCount = 1
 		# adding up overall scores
 		var sum = 0
 		for i in Global.arr.size():
 			sum += Global.arr[i]
 		Global.overallScore = sum
-		
-		# change scene to game end screen when level cap is hit
-		Global.levelCount = 1
 		string = "res://Scenes/End/endgame.tscn"
 		get_tree().change_scene_to_file(string)
 	else:
